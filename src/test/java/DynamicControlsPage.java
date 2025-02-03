5f4239178bac90b3b9fbdb8b4f52aa339dcbf4a2
Kajol <kajolgupta994@gmail.com>
2025-02-03 18:49:08 +0530

    Working on Dynamic controls page

---
+++ src/test/java/DynamicControlsPage.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DynamicControlsPage {
+	private WebDriver driver;
+	@FindBy(id = "checkbox")
+	private WebElement checkbox;
+	@FindBy(xpath = "//button[text()='Remove']")
+	private WebElement removeButton;
+	@FindBy(xpath = "//button[text()='Enable']")
+	private WebElement enableButton;
+	@FindBy(xpath = "//form[@id='input-example']/input[@type='text']")
+	private WebElement inputBox;
+	@FindBy(xpath = "//button[text()='Disable']")
+	private WebElement disableButton;
+	@FindBy(id = "message")
+	private WebElement message; // It's disabled! or It's enabled!
+
+	public DynamicControlsPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+}
