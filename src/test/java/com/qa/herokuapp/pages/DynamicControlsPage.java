b489b8538d4343f72e7a4af89da08b4bfd4ac600
Kajol <kajolgupta994@gmail.com>
2025-02-04 20:36:32 +0530

    Removed unused imports

--- src/test/java/com/qa/herokuapp/pages/DynamicControlsPage.java
+++ src/test/java/com/qa/herokuapp/pages/DynamicControlsPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 import com.qa.herokuapp.base.TestBase;
 import com.qa.herokuapp.helper.WebDriverWaitUtils;
