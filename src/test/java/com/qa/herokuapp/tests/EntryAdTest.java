348754a7cf1ad10df8c4465f61c72de85ca1e01e
Kajol <kajolgupta994@gmail.com>
2025-02-05 18:07:09 +0530

    Completed testing of the Entry Ad page

---
+++ src/test/java/com/qa/herokuapp/tests/EntryAdTest.java
@@ -0,0 +1,34 @@
+package com.qa.herokuapp.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import com.qa.herokuapp.base.TestBase;
+import com.qa.herokuapp.pages.EntryAdPage;
+import com.qa.herokuapp.pages.HomePage;
+
+public class EntryAdTest extends TestBase {
+	private EntryAdPage entryAdPage;
+	private HomePage homePage;
+
+	@BeforeMethod
+	public void setup() {
+		homePage = new HomePage(getDriver());
+		entryAdPage = new EntryAdPage(getDriver());
+	}
+
+	@Test(priority = 1)
+	public void testCloseModal() {
+		LOGGER.info("Navigating to Entry Ad Page...");
+        homePage.clickOnEntryAdLinkText();
+        LOGGER.info("Successfully navigated to Entry Ad Page.");
+
+		LOGGER.info("Testing modal close functionality...");
+		entryAdPage.closeModal();
+		
+		LOGGER.info("Testing modal re-enable functionality...");
+		entryAdPage.reEnableModal();
+		
+	}
+
+}
